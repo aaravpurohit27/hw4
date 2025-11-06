730047e1fc0a2eb4600d692d6695cf3d1e481c77
Aarav Purohit <aaravpur@usc.edu>
2025-11-06 21:02:49 +0000

    Added function in equal-paths.cpp and equal-paths.h to check if a tree has paths of equal length using recursion and a helper function

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -12,7 +12,51 @@ using namespace std;
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  // Add your code below
+  bool equal = false;
+  if(root == nullptr){
+    equal = true;
+  }
+  else if(root->left == nullptr && root->right == nullptr){
+    equal = true;
+  }
+  else if(root->left == nullptr){
+    if(get_height(root->right)==1){
+      equal = true;
+    }
+    else{
+      equal = false;
+    }
+  }
+  else if(root->right == nullptr){
+    if(get_height(root->left)==1){
+      equal = true;
+    }
+    else{
+      equal = false;
+    }
+  }
+  else if(get_height(root->left)-get_height(root->right)==0){
+    equal = true;
+  }
+  else{
+    equal = false;
+  }
+  return equal;
+}
 
+//helper funct
+int get_height(Node * root){
+  if(root == nullptr){
+    return 0;
+  }
+  else{
+    if(get_height(root->left)>get_height(root->right)){
+      return 1+ get_height(root->left);
+    }
+    else{
+      return 1+ get_height(root->right);
+    }
+  }  
 }
 
